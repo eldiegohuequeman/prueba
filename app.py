bd5f915dfcf5c2d4c485774ae06f67bf656baba7
Claudia reyes jofre <creyes@abz.cl>
2023-11-11 15:44:24 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '¡Hola, mundo! esta es la prueba.'
+
+if __name__ == '__main__':
+    app.run(debug=True)

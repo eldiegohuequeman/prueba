5ca92e9d56a01d4d6f2ee4c7b26a6458e341ae30
Diego Huequeman Gallardo <dhuequeman@pronovasalud.com>
2023-11-12 21:44:26 -0300

    prueba profile4

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ def hello():
 if __name__ == '__main__':
     # Obtén el puerto de la variable de entorno o usa 5000 si no está disponible
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, port=port)
+    app.run(debug=True, port=port, host='0.0.0.0')
